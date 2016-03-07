930b45cc58bbcdb3b4dcd1b105fd185ff014ee12
bkchan <bryantchan@gmail.com>
2016-03-06 19:36:39 -0800

    Create _client.py

---
+++ _client.py
@@ -0,0 +1,33 @@
+import json
+import subprocess
+from slackclient import SlackClient
+
+class MySlackClient(SlackClient):
+
+    def __init__(self, token):
+        super(MySlackClient, self).__init__(token)
+
+    @staticmethod
+    def _run_command(command):
+        p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+        return iter(p.stdout.readline, b'')
+
+    def post_message(self, channel, text):
+        self.api_call('chat.postMessage', channel = channel, text = text, as_user = True, unfurl_links = False, link_names = True)
+
+    def show_is_typing(self, channel):
+        self.server.send_to_websocket({'type': 'typing', 'channel': channel, 'id': 1})
+
+    def get_user(self, user):
+        try:
+            json_data = json.loads(self.api_call('users.info', user = user))
+            if 'ok' in json_data and json_data['ok'] and 'user' in json_data and 'name' in json_data['user']:
+                return json_data['user']['name']
+        except Exception as e:
+            pass
+        return user
+
+    def upload_file(self, channel, filename):
+        command = 'curl -F file=@' + filename + ' -F channels=' + channel + ' -F token=' + self.token + ' https://slack.com/api/files.upload'
+        self._run_command(command.split())
+
